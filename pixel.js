9ce91c59ac4aaa3b93308923e0581b6501e759f0
Ramin Zamani <ramin@technomarketers.com>
2025-02-26 08:39:51 -0800

    fixed the bug: shardKey params was named as purl. While the value passed is the purl, the paramater name should be shardKey.

--- pixel.js
+++ pixel.js
@@ -3,7 +3,7 @@
   const edgeServiceURI = "https://services.mdl.io";
 
   /**
-   * Helper function that wraps the native fetch to make a JSON request and return the resolved the data object from response.
+   * Helper function that wraps the native fetch to make a JSON request and return the resolved data object from response.
    * @param {String} url
    * @param {Object} options
    * @returns {Object}
@@ -27,22 +27,24 @@
 
   /**
    * Authenticates a record with MindFire Identity Service and returns the record with a few added utility functions.
-   * @param {String} aid      MindFire Account ID with remote access enabled for the domain/IP address.
-   * @param {Object} filter   Query parameters to retrieve the record by.
+   * @param {String} aid      MindFire Account ID with Remote Access enabled for the domain/IP address.
+   * @param {Object} filter   Query parameters to retrieve the record by, e.g. { email: "test@test.com" }.
    * @param {Number} expiry   JWT tokem expiration time in seconds.
-   * @param {String} purl     PURL value of the record.
+   * @param {String} shardKey Pass the PURL value of the record which be used as the shardKey.
    * @returns {Promise}       The authenticated record decorated by helper functions for convenience.
    *
    */
-  _MFS.getAuthenticatedRecord = async function (aid, filter, expiry, purl) {
+  _MFS.getAuthenticatedRecord = async function (aid, filter, expiry, shardKey) {
     const url = `${identityServiceURI}/api/identity/v1/list-record-auth`;
     const options = {
       method: "POST",
-      body: JSON.stringify({ aid, filter, expiry, purl }),
+      body: JSON.stringify({ aid, filter, expiry, shardKey }),
     };
     const record = await callApi(url, options);
+
+    // Helper function to update the record
     record.update = async function (params) {
-      params.purl = params.purl || purl; // Add shardKey if not provided;
+      params.shardKey = params.shardKey || shardKey; // Add the shardKey if not provided;
       const url = `${edgeServiceURI}/api/contact-service/v1/offers/${record.data._id}`;
       const options = {
         method: "PUT",
@@ -53,8 +55,10 @@
       };
       return await callApi(url, options);
     }
+
+    // Helper function to add an event to the record  
     record.addEvent = async function (params) {
-      params.purl = params.purl || purl; // Add shardKey if not provided;
+      params.shardKey = params.shardKey || shardKey; // Add shardKey if not provided;
       const url = `${edgeServiceURI}/api/contact-service/v1/events/${record.data._id}`;
       const options = {
         method: "PUT",
@@ -65,15 +69,16 @@
       };
       return await callApi(url, options);
     };
+    
     return record;
   };
 
-  
+
   /**
    * Returns a record only with public data as defined by remote access configs.
    * @param {String} aid      MindFire Account ID with remote access enabled for the domain/IP address.
-   * @param {String} purl     The PURL of the record
-   * @returns {Promise}       The record only with the defined public data.
+   * @param {String} purl     The PURL value of the record.
+   * @returns {Promise}       The record with the defined public data in the Remote Access config.
    *
    */
   _MFS.getRecordPublicData = async function (aid = _mfaid, purl = _mfpurl) {
@@ -86,15 +91,15 @@
 
   /**
    * Wraps the _MFS.getAuthenticatedRecord method to provide default options for convenience.
-   * @param {String} aid      MindFire Account ID with remote access enabled for the domain/IP address.
-   * @param {Object} filter   Query parameters to retrieve the record by.
+   * @param {String} aid      MindFire Account ID with Remote Access enabled for the domain/IP address.
+   * @param {Object} filter   Query parameters to retrieve the record by, e.g. { email: "test@test.com" }.
    * @param {Number} expiry   JWT tokem expiration time in seconds.
-   * @param {String} shardKey 
-   * @returns {Promise}        The authenticated record decorated by helper functions for convenience.
+   * @param {String} shardKey The PURL value of the record is used as the shardKey. 
+   * @returns {Promise}       The authenticated record decorated by helper functions for convenience.
    *
    */
-  _MFS.getRecord = async function (aid = _mfaid, filter = { purl: _mfpurl }, expiry = 60, purl = _mfpurl) {
-    return await _MFS.getAuthenticatedRecord(aid, filter, expiry, purl);
+  _MFS.getRecord = async function (aid = _mfaid, filter = { purl: _mfpurl }, expiry = 60, shardKey = _mfpurl) {
+    return await _MFS.getAuthenticatedRecord(aid, filter, expiry, shardKey);
   }
 
 })(
